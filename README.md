437fdf3671fa8c50e7c1aa32a2ae65252e8e19cf
Kurotwo <kurt_bradley_tanting@dlsu.edu.ph>
2020-10-05 13:46:26 +0800

    Updated readme and piplock file

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
+### Update 
+Course offerings uses cloudflare which disables scraping
+
 # animo-scraper
 Gets courses from DLSU Course Offerings and saves the results into a excel file. 
+
+## How to run 
+1. pip install pipenv
+2. pipenv sync 
+3. pipenv run python scraper.py 
--- scraper.py
+++ scraper.py
@@ -8,17 +8,18 @@ import time
 
 # Specify webpage browser to use 
 PATH = "C:\Program Files (x86)\chromedriver.exe"
-driver = webdriver.Chrome(PATH) 
+driver = webdriver.Chrome(executable_path=PATH) 
 # Specify page url 
-driver.get("https://enroll.dlsu.edu.ph/dlsu/view_actual_count")
+# driver.get("https://enroll.dlsu.edu.ph/dlsu/view_actual_count")
+driver.get("http://enroll.dlsu.edu.ph/dlsu/view_actual_count?__cf_chl_jschl_tk__=9fa7dba2172ad594812fe77e4878de65bb395ad6-1600226317-0-ASZO5sIX1IJQ3mOnmxNBfP7rbB8Uad4EfSkGJuUKyN4Lur32_mhzK-TBExC83duqH3oDAERYMxWL98M-PInlgJgOnT8CgHAGZpSbuglz2AihkV8qi1NKaqstjdA2OWmYxeGyxS7ahbm0qlAmRWLW5CWgQ9ykznWwzDhyujtPQsBumV-MNorZCsbvgKwhGQv2AtbZKB-tvA3XQE-RcYByqcf5eAbSt9Pvdfw4RmqRc315gMuanI0M9mU8AxPoRysF9WZT4HQYwLpYeqYsF7ruOGocZFpkCVwRTORYm5FXlL5T")
 # Specify course codes 
-course_codes = ["GEWORLD", "CSNETWK", "CSSWENG", "GELITPH", "CSMODEL", "GERIZAL"]
+course_codes = ["STSWENG", "STADVDB", "CSARCH2", "LBYARCH", "STHCIUX", "DATA100", "GEWORLD"]
 try:
     #Save the course tables into master table 
     table_df = []
     for course in course_codes: 
         # Wait until the search bar is available 
-        search_field = WebDriverWait(driver, 30).until(
+        search_field = WebDriverWait(driver, 60).until(
             EC.presence_of_element_located((By.NAME, "p_course_code"))
         )
 
